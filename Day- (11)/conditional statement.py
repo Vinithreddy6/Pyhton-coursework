704bfe7e48a653981eaaa78f3ae34c06809dbd5a
Vinithreddy6 <vinithmarepally@gmail.com>
2026-08-21 17:56:33 +0530

    Add files via upload

---
+++ Day- (11)/conditional statement.py
@@ -0,0 +1,24 @@
+conditional statement
+--------------
+1.simpleif
+2.if else
+3.elif ladder
+4.nested if
+-------------
+if cond:
+  #stmts
+-------------
+ifcod:
+  #stms t
+else:
+  #stms f
+-------------
+if cond1:
+  #statement1
+else cond2:
+  #statement2
+...........
+else(opt):
+  #sratement
+.............
+nested if      
